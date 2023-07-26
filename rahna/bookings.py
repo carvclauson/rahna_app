73e60e679833d8b9cd1319eef9deb91ffb3d50a5
Clauson Silva <clausoncarvalho@hotmail.com>
2023-07-26 18:40:45 +0200

    fixed the identation in bookings.py

--- rahna/bookings.py
+++ rahna/bookings.py
@@ -17,7 +17,7 @@ class Booking(BaseModel):
     total_days: timedelta = None
     price: float = 0.0
     booking_confirmed: str = ""
-    
+
 @classmethod
 def total_days(start_date, end_date):
     # Return timedelta in days
@@ -32,111 +32,111 @@ def create_table(cls, database_name = "../data/bookings.db"):
     conn = sqlite3.connect(database_name)
 
     conn.execute(
-        """CREATE TABLE IF NOT EXISTS bookings (id TEXT, name TEXT, 
-        parent_name: TEXT, start_date DATETIME, end_date DATETIME, 
+        """CREATE TABLE IF NOT EXISTS bookings (id TEXT, name TEXT,
+        parent_name: TEXT, start_date DATETIME, end_date DATETIME,
         total_days INT, price FLOAT)"""
     )
     conn.close()
-    
-    
-    def save(self, database_name = "../data/bookings.db")-> "Booking":
-        """
-        Inserts a row in a database with path database_name. Returns the Booking
-        object that called the function (self).
-        """
 
-        with sqlite3.connect(os.getenv("DATABASE_NAME", database_name)) as con:
-            cur = con.cursor()
-            cur.execute(
-                f""" INSERT INTO bookings (id, dog_id, name, parent_name, 
-                start_date, end_date, total_days, price) 
-                VALUES('{self.id}','{Dog.id}', {Dog.name}','{Dog.parent_name}',
-                '{self.start_date}','{self.end_date}','{self.total_days}', '{self.price}')"""
-            )
-            con.commit()
-        return self
-    
 
-    @classmethod
-    def current_bookings(cls, database_name = "../data/bookings.db") -> List["Dog"]:
-        """
-        Lists rows in a database with path database_name. 
-        Returns a list of current and future bookings.
-        """
+def save(self, database_name = "../data/bookings.db")-> "Booking":
+    """
+    Inserts a row in a database with path database_name. Returns the Booking
+    object that called the function (self).
+    """
 
-        con = sqlite3.connect(os.getenv("DATABASE_NAME", database_name))
-        con.row_factory = sqlite3.Row
-
+    with sqlite3.connect(os.getenv("DATABASE_NAME", database_name)) as con:
         cur = con.cursor()
-        cur.execute("SELECT * FROM bookings WHERE end_date < DATE('now')")
-
-        records = cur.fetchall()
-        bookings = [cls(**record) for record in records]
-        con.close()
-
-        return bookings
-    
-    @classmethod
-    def get_booking_by_id(cls, booking_id: str, database_name = "../data/bookings.db"):
-        """
-        Selects booking with booking_id as id in a database with path database_name
-        and returns it.
-        """
-
-        con = sqlite3.connect(os.getenv("DATABASE_NAME", database_name))
-        con.row_factory = sqlite3.Row
-
-        cur = con.cursor()
-        cur.execute(f"SELECT * FROM dogs WHERE id='{booking_id}'")
-
-        record = cur.fetchone()
-
-        if record is None:
-            raise NotFound
-
-        booking = cls(**record)
-        con.close()
-        return booking
-    
-    @classmethod
-    def get_booking_by_name(cls, dog_name: str, database_name = "../data/bookings.db"):
-        """
-        Selects booking with dog_name as name in a database with path database_name
-        and returns it.
-        """
-
-        con = sqlite3.connect(os.getenv("DATABASE_NAME", database_name))
-
-        con.row_factory = sqlite3.Row
-
-        cur = con.cursor()
-        cur.execute(f"SELECT * FROM dogs WHERE name = '{dog_name}'")
-
-        record = cur.fetchone()
-
-        if record is None:
-            raise NotFound
-
-        booking = cls(**record)
-        con.close()
-        return booking
-
-    @classmethod
-    def delete_booking_by_name(cls, dog_name: str, database_name = "../data/bookings.db"):
-        """
-        Deletes bookingg with dog_name as name from the database with path database_name
-        and returns it.
-        """
-
-        con = sqlite3.connect(os.getenv("DATABASE_NAME", database_name))
-
-        cur = con.cursor()
-        cur.execute(f"""
-                    DELETE
-                    FROM bookings
-                    WHERE name = '{dog_name}'
-                    """)
-
+        cur.execute(
+            f""" INSERT INTO bookings (id, dog_id, name, parent_name,
+            start_date, end_date, total_days, price)
+            VALUES('{self.id}','{Dog.id}', {Dog.name}','{Dog.parent_name}',
+            '{self.start_date}','{self.end_date}','{self.total_days}', '{self.price}')"""
+        )
         con.commit()
+    return self
 
-        return None
+
+@classmethod
+def current_bookings(cls, database_name = "../data/bookings.db") -> List["Dog"]:
+    """
+    Lists rows in a database with path database_name.
+    Returns a list of current and future bookings.
+    """
+
+    con = sqlite3.connect(os.getenv("DATABASE_NAME", database_name))
+    con.row_factory = sqlite3.Row
+
+    cur = con.cursor()
+    cur.execute("SELECT * FROM bookings WHERE end_date < DATE('now')")
+
+    records = cur.fetchall()
+    bookings = [cls(**record) for record in records]
+    con.close()
+
+    return bookings
+
+@classmethod
+def get_booking_by_id(cls, booking_id: str, database_name = "../data/bookings.db"):
+    """
+    Selects booking with booking_id as id in a database with path database_name
+    and returns it.
+    """
+
+    con = sqlite3.connect(os.getenv("DATABASE_NAME", database_name))
+    con.row_factory = sqlite3.Row
+
+    cur = con.cursor()
+    cur.execute(f"SELECT * FROM dogs WHERE id='{booking_id}'")
+
+    record = cur.fetchone()
+
+    if record is None:
+        raise NotFound
+
+    booking = cls(**record)
+    con.close()
+    return booking
+
+@classmethod
+def get_booking_by_name(cls, dog_name: str, database_name = "../data/bookings.db"):
+    """
+    Selects booking with dog_name as name in a database with path database_name
+    and returns it.
+    """
+
+    con = sqlite3.connect(os.getenv("DATABASE_NAME", database_name))
+
+    con.row_factory = sqlite3.Row
+
+    cur = con.cursor()
+    cur.execute(f"SELECT * FROM dogs WHERE name = '{dog_name}'")
+
+    record = cur.fetchone()
+
+    if record is None:
+        raise NotFound
+
+    booking = cls(**record)
+    con.close()
+    return booking
+
+@classmethod
+def delete_booking_by_name(cls, dog_name: str, database_name = "../data/bookings.db"):
+    """
+    Deletes bookingg with dog_name as name from the database with path database_name
+    and returns it.
+    """
+
+    con = sqlite3.connect(os.getenv("DATABASE_NAME", database_name))
+
+    cur = con.cursor()
+    cur.execute(f"""
+                DELETE
+                FROM bookings
+                WHERE name = '{dog_name}'
+                """)
+
+    con.commit()
+
+    return None
